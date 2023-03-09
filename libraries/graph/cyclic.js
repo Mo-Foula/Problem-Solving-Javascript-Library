e6aa73fd94f8aa1eff16458f1887045c4fee0997
Mohamed Foula <mohamed.m.foula@gmail.com>
2023-03-10 01:33:35 +0200

    feat(library): graph cycles

---
+++ libraries/graph/cyclic.js
@@ -0,0 +1,28 @@
+const hasCycle = function(head) {
+    if (!head || !head.next) return null
+    let slowPtr = head, fastPtr = head
+    while(fastPtr && fastPtr.next){
+        slowPtr = slowPtr.next
+        fastPtr = fastPtr.next.next
+        if (fastPtr == slowPtr){
+            return true
+        }
+    }
+    return false
+};
+
+const detectCycleStart = function(head){
+    let intersection = hasCycle(head)
+    if(!intersection) return null
+    let slowPtr = head
+    while(slowPtr !== intersection){
+        slowPtr = slowPtr.next
+        intersection = intersection.next
+    }
+    return intersection
+}
+
+module.exports = {
+    hasCycle,
+    detectCycleStart
+}
